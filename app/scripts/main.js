462eb0e7cef4880f149d02166f817d098a048498
vinh <tranvinh@hotmail.com.vn>
2018-04-10 23:25:36 +0700

    second commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+// console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows: true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+  });
+});
