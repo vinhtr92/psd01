e82a0fb324ebf11630bb2a8cbd65bc9ece7c6988
vinh <tranvinh@hotmail.com.vn>
2018-04-12 19:06:54 +0700

    mobile menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,4 +6,26 @@ $(document).ready(function(){
     infinite: true,
     speed: 500,
   });
+
+
+  const newDiv = $("<div class='main-nav'><div class='row'></div></div>")
+  const newDiv2 = $("<div class='main-logo'><div class='row'></div></div>")
+
+
+  $(".main-menu__btn").click(function(){
+    $(".main-menu__nav").slideToggle("fast")
+  });
+
+  if(screen.width >= 768){
+    $(".main-menu__btn").remove();
+    // $(".main-menu__logo img").remove();
+    $(".top-links").appendTo($(".main-menu"));
+    $(".social").appendTo($(".main-menu"));
+    $(".main-menu__logo img").appendTo(".main-logo");
+
+
+
+  };
 });
+
+
