61751024d0184c5d7ee809091d6d222f5562d85d
vinh <tranvinh@hotmail.com.vn>
2018-04-13 11:17:08 +0700

    them active class

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,48 @@
 // console.log('\'Allo \'Allo!');
 $(document).ready(function(){
   $('.slider').slick({
-    arrows: true,
+    arrows: false,
     dots: true,
     infinite: true,
     speed: 500,
   });
 
 
-  const newDiv = $("<div class='main-nav'><div class='row'></div></div>")
-  const newDiv2 = $("<div class='main-logo'><div class='row'></div></div>")
+  const newDiv = $("<div class='main-nav'><div class='row'><div class='col-12'></div></div>")
+
+  const newDiv2 = $("<div class='main-logo'><div class='row'><div class='col-md-6'></div><div class='col-md-6'></div></div></div>")
+
+  const newDiv3 = $("<div class='row'><div class='col-md-6'></div><div class='col-md-6'></div></div>")
 
 
   $(".main-menu__btn").click(function(){
     $(".main-menu__nav").slideToggle("fast")
   });
 
+
+
   if(screen.width >= 768){
+    $(".top-bar").after(newDiv,newDiv2);
+    $(".main-menu").prepend(newDiv3);
+
     $(".main-menu__btn").remove();
+    // $(".main-menu__nav").remove();
     // $(".main-menu__logo img").remove();
-    $(".top-links").appendTo($(".main-menu"));
-    $(".social").appendTo($(".main-menu"));
-    $(".main-menu__logo img").appendTo(".main-logo");
+    $(".top-links").appendTo($(".main-menu .row .col-md-6:nth-child(1)"));
 
+    $(".social").appendTo($(".main-menu .row .col-md-6:nth-child(2)")).addClass("float-md-right");
 
+    $(".main-menu__logo").appendTo(".main-logo .row .col-md-6:nth-child(1)");
 
+    $(".search-form").appendTo(".main-logo .row .col-md-6:nth-child(2)");
+
+    $(".main-menu__nav").appendTo(".main-nav .row .col-12" );
+
+    $(".main-links li").click(function(){
+      $(".main-links li ").removeClass("is-active");
+      $(this).addClass("is-active");
+
+    })
   };
 });
 
